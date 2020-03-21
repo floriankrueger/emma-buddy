2a8eee4929f7a4262333cb379444c6c4d4122ad6
Florian Krüger <hi@spreekind.io>
2020-03-21 09:38:42 +0100

    install firebase SDK, inject config through env

--- src/main.js
+++ src/main.js
@@ -2,6 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './routes';
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APP_API_KEY,
+  authDomain: process.env.FIREBASE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_APP_DATABASE_URL,
+  projectId: process.env.FIREBASE_APP_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_APP_ID
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+let app = firebase.app()
+let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function')
+console.log(`Firebase SDK loaded with ${features.join(', ')}`)
 
 Vue.use(VueRouter)
 
