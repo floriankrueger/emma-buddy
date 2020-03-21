86d973116db264b49548c0df27e3002ec9a787f7
Simon Iser <simon_iser@yahoo.com>
2020-03-21 14:57:43 +0100

    updated TODOs

--- src/components/Aktuelles.vue
+++ src/components/Aktuelles.vue
@@ -25,7 +25,7 @@ export default {
     components: {
         Navbar
     },
-    data(){
+    data(){/** TODO: Anbinden an firebase */
         return {
             meldungen: [
                 {
