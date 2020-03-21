541688359c63a6cc05a22cfab370f42587a82dfd
Florian Krüger <hi@spreekind.io>
2020-03-21 16:27:25 +0100

    show buddy list

--- src/components/Berater.vue
+++ src/components/Berater.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>
-        Berater
+    <div class="container-fluid" id="berater">
+        <h1>Berater</h1>
+        <div v-for="buddy in buddies" :key="buddy.bid">
+            <h2>{{ buddy.name }}</h2>
+            <p>{{ buddy.standort }}</p>
+        </div>
     </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 export default {
-    
+    name: 'Berater',
+    computed: {
+        ...mapState(['buddies'])
+    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 
 import VueRouter from "vue-router"
 
-import { BootstrapVue, IconsPlugin, FormRadioPlugin } from "bootstrap-vue"
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
